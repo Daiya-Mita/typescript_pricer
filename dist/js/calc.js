5bf7e16463711034d50193bea3d94f2cdbde379d
Daiya Mita <daiyamita@Daiya-MacBook-Pro.local>
2020-05-02 17:16:43 +0900

    関数化

--- dist/js/calc.js
+++ dist/js/calc.js
@@ -27,26 +27,31 @@ function function_parser_sample(func_param) {
         return a[target_grid] * x + b[target_grid];
     };
 }
-// 以下の変数は外部から取ってくることを想定
-var spot = 100;
-var vol = 0.1;
-var expiry = 1;
-var r = 0.01;
-// 正規分布のプラマイ5σ幅でリーマン積分します
-var mesh = 1000;
-var rnd_domain_min = -5;
-var rnd_domain_max = 5;
-var step_delta = (rnd_domain_max - rnd_domain_min) / mesh;
+var spot = 130; // 外部から取ってくることを想定
+var vol = 0.8; // 外部から取ってくることを想定
+var expiry_date = new Date('2020/06/17 12:34:56');
+var expiry_date_unix = Math.floor(expiry_date.getTime() / 1000); // 外部から取ってくることを想定
+var now = new Date();
+var now_unix = Math.floor(now.getTime() / 1000);
+var expiry = (expiry_date_unix - now_unix) / (365 * 60 * 60 * 24); // 非負制約はあった方が良いかも
+var r = 0;
 // function mapping,とりあえず 0-100まで傾き1でその後フラットになる関数を想定
 var func_1_param = ['00000100:00010000', '01009999:00000100'];
-var payoff_func = function_parser_sample(func_1_param);
-var runningSum = 0;
-for (var i = 0; i < mesh; i++) {
-    var x = rnd_domain_min + i * step_delta;
-    var moved_spot = spot * Math.exp(r * expiry - 0.5 * vol * vol * expiry + vol * Math.sqrt(expiry) * x);
-    //var payoff:number = Math.max(0, moved_spot - 50);  // payoff関数がmaxになってるとコールオプション
-    var payoff = payoff_func(moved_spot);
-    runningSum += payoff * pdf(x) * step_delta;
+var payoff_func = function_parser_sample(func_1_param); // 外部から取ってくることを想定
+function deriv_pricer(spot, vol, expiry, r, payoff_func) {
+    var mesh = 1000;
+    var rnd_domain_min = -5;
+    var rnd_domain_max = 5;
+    var step_delta = (rnd_domain_max - rnd_domain_min) / mesh;
+    var runningSum = 0;
+    for (var i = 0; i < mesh; i++) {
+        var x = rnd_domain_min + i * step_delta;
+        var moved_spot = spot * Math.exp(r * expiry - 0.5 * vol * vol * expiry + vol * Math.sqrt(expiry) * x);
+        //var payoff:number = Math.max(0, moved_spot - 50);  // payoff関数がmaxになってるとコールオプション
+        var payoff = payoff_func(moved_spot);
+        runningSum += payoff * pdf(x) * step_delta;
+    }
+    var ans = runningSum * Math.exp(-r * expiry);
+    return ans;
 }
-var ans = runningSum * Math.exp(-r * expiry);
-console.log(ans);
+console.log(deriv_pricer(spot, vol, expiry, r, payoff_func));
--- src/ts/calc.ts
+++ src/ts/calc.ts
@@ -28,29 +28,37 @@ function function_parser_sample(func_param: String[]){
     }
 }
 
-// 以下の変数は外部から取ってくることを想定
-const spot:number = 100;
-const vol:number = 0.1;
-const expiry:number = 1;
-const r:number = 0.01;
 
-// 正規分布のプラマイ5σ幅でリーマン積分します
-const mesh:number = 1000;
-const rnd_domain_min:number = -5;
-const rnd_domain_max:number = 5;
-const step_delta:number = (rnd_domain_max - rnd_domain_min)/mesh;
-
+const spot:number = 130;  // 外部から取ってくることを想定
+const vol:number = 0.8;  // 外部から取ってくることを想定
+const expiry_date = new Date('2020/06/17 12:34:56');
+const expiry_date_unix = Math.floor(expiry_date.getTime()/1000);  // 外部から取ってくることを想定
+const now = new Date();
+const now_unix = Math.floor(now.getTime()/1000);
+const expiry:number = (expiry_date_unix - now_unix)/(365*60*60*24);  // 非負制約はあった方が良いかも
+const r:number = 0;
 // function mapping,とりあえず 0-100まで傾き1でその後フラットになる関数を想定
 const func_1_param = ['00000100:00010000', '01009999:00000100'];  
-let payoff_func = function_parser_sample(func_1_param)
+let payoff_func = function_parser_sample(func_1_param)  // 外部から取ってくることを想定
 
-let runningSum:number = 0;
-for (var i = 0; i < mesh; i++) {
-    var x:number = rnd_domain_min + i * step_delta;
-    var moved_spot:number = spot*Math.exp(r*expiry-0.5*vol*vol*expiry+vol*Math.sqrt(expiry)*x);
-    //var payoff:number = Math.max(0, moved_spot - 50);  // payoff関数がmaxになってるとコールオプション
-    var payoff:number = payoff_func(moved_spot)
-    runningSum += payoff * pdf(x) * step_delta;
+
+
+function deriv_pricer(spot: number, vol: number, expiry: number, r:number, payoff_func: (x:number) =>number){
+    const mesh:number = 1000;
+    const rnd_domain_min:number = -5;
+    const rnd_domain_max:number = 5;
+    const step_delta:number = (rnd_domain_max - rnd_domain_min)/mesh;
+
+    let runningSum:number = 0;
+    for (var i = 0; i < mesh; i++) {
+        var x:number = rnd_domain_min + i * step_delta;
+        var moved_spot:number = spot*Math.exp(r*expiry-0.5*vol*vol*expiry+vol*Math.sqrt(expiry)*x);
+        //var payoff:number = Math.max(0, moved_spot - 50);  // payoff関数がmaxになってるとコールオプション
+        var payoff:number = payoff_func(moved_spot)
+        runningSum += payoff * pdf(x) * step_delta;
+    }
+    let ans:number = runningSum * Math.exp(-r*expiry);
+    return ans;
 }
-let ans:number = runningSum * Math.exp(-r*expiry);
-console.log(ans);
+
+console.log(deriv_pricer(spot, vol, expiry, r, payoff_func));
